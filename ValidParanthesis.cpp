9be3077e9b3da504903f136205745ea02dff1a95
Mukala Sai Charan Kumar <75615707+saicharan21-dev@users.noreply.github.com>
2023-10-15 09:53:19 +0530

    Create ValidParanthesis.cpp

---
+++ ValidParanthesis.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    bool isValid(string str){
+	stack<char>s;
+	for(int i=0;i<str.length();i++){
+		if(s.empty()){
+			s.push(str[i]);
+		}
+		else if((s.top()=='('&& str[i]==')')
+				||(s.top()=='{'&&str[i]=='}')
+				||(s.top()=='['&&str[i]==']')) {
+					s.pop();
+				}
+		else{
+			s.push(str[i]);
+		}
+				
+	}
+	if(s.empty()){
+		return true;
+	}
+	return false;
+}
+};
