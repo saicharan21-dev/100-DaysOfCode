11fa41c5b1546f086865b24807f3b2761179ba45
Mukala Sai Charan Kumar <75615707+saicharan21-dev@users.noreply.github.com>
2023-10-17 22:16:51 +0530

    Create ContainsDuplicates3.cpp

---
+++ ContainsDuplicates3.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    bool containsNearbyAlmostDuplicate(vector<int>& nums, int indexDiff, int valueDiff) {
+        int n=nums.size();
+        set<int>st(nums.begin(),nums.end());
+        if(valueDiff==0 && st.size()==n) return false;
+        for(int i=0;i<n;i++){
+            for(int j=i+1;j<i+1+indexDiff;j++){
+                if(j>=n) break;
+                if(abs((long long)nums[i]-nums[j])<=valueDiff) return true;
+            }
+        }
+        return false;
+    }
+};
