532a0a5f9bf13c0c1a99cf45dae3b503791c31d1
Mukala Sai Charan Kumar <75615707+saicharan21-dev@users.noreply.github.com>
2023-10-19 22:19:37 +0530

    Create Backspace String Compare.cpp

---
+++ Backspace String Compare.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    bool backspaceCompare(string s, string t) {
+        string vec1, vec2;
+
+        for (int i = 0; i < s.length(); i++) {
+            if (s[i] == '#') {
+                if (!vec1.empty()) {
+                    vec1.pop_back();
+                }
+            } else {
+                vec1.push_back(s[i]);
+            }
+        }
+
+        for (int i = 0; i < t.length(); i++) {
+            if (t[i] == '#') {
+                if (!vec2.empty()) {
+                    vec2.pop_back();
+                }
+            } else {
+                vec2.push_back(t[i]);
+            }
+        }
+
+        return vec1 == vec2;
+    }
+};
